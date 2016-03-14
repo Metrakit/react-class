e834c1abae6121991af2c788f60468c253068c97
JOUFFROY Jordane <j.jouffroy@forprodis.fr>
2016-03-14 13:34:03 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp       = require('gulp'),
+	browserify = require('browserify'),
+	babelify   = require('babelify'),
+	source     = require('vinyl-source-stream');
+
+var src_path  = "src/";
+var dist_path = "dist/";
+
+gulp.task('scripts', function () {
+    return browserify({
+    		entries: src_path + 'js/app.jsx', 
+    		extensions: ['.jsx'], 
+    		debug: true
+    	})
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(dist_path + 'js/'));
+});
+
+// todo
+gulp.task('scss', function() {
+	// gulp.src(src_path + 'scss/*.scss')
+	// .pipe(plumber(plumberErrorHandler))
+	// .pipe(sourcemaps.init())
+	// .pipe(sass({
+	// 	includePaths: bourbon.includePaths
+	// }))
+	// .pipe(sourcemaps.write())
+	// .pipe(concat('app.css'))
+	// .pipe(gulpif(argv.production, uncss({
+	//     html: [url]
+	// })))
+	// .pipe(gulp.dest(dist_path + 'css'))
+	// .pipe(reload({stream: true}));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('*.jsx', ['scripts']);
+    gulp.watch('*.scss', ['scss']);
+});
+
+gulp.task('default', ['scripts', 'scss']);
